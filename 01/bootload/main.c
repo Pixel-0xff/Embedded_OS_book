754b49fa00ca22532d4369bd33b16a21a3942a29
Pixel-0xff <ht19a022@oecu.jp>
2022-05-28 22:35:19 +0900

    追加した16進表示関数を追加

--- 01/bootload/main.c
+++ 01/bootload/main.c
@@ -2,12 +2,15 @@
 #include "serial.h"
 #include "lib.h"
 
-int main(void){
+int main(void) {
 	serial_init(SERIAL_DEFAULT_DEVICE);
 
 	puts("Hello World!\n");
 
-	while(1);
-	
+	putxval(0x10, 0);	puts("\n");
+	putxval(0xffff, 0);	puts("\n");
+
+	while (1);
+
 	return 0;
 }
