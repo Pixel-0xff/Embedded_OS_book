2cd3bd67e83ecd82c67f81eef7e537fa1ed09af6
Pixel-0xff <ht19a022@oecu.jp>
2022-06-01 22:43:20 +0900

    runコマンドの追加

--- 01/bootload/main.c
+++ 01/bootload/main.c
@@ -1,18 +1,7 @@
 #include "defines.h"
 #include "serial.h"
+#include "xmodem.h"
 #include "lib.h"
-
-int global_data = 0x10;
-int global_bss;
-static int static_data = 0x20;
-static int static_bss;
-
-static void printval(void) {
-	puts("global_data = "); putxval(global_data, 0);	puts("\n");
-	puts("global_bss = ");	putxval(global_bss, 0);		puts("\n");
-	puts("static_data = ");	putxval(static_data, 0);	puts("\n");
-	puts("static_bss = ");	putxval(static_bss, 0);		puts("\n");
-}
 
 static int init(void) {
 	/* リンカスクリプトで定義したシンボル */
@@ -28,20 +17,75 @@ static int init(void) {
 	return 0;
 }
 
+/* メモリの16進数ダンプ出力 */
+static int dump(char *buf, long size) {
+	long i;
+
+	if (size < 0) {
+		puts("no data.\n");
+		return -1;
+	}
+
+	for (i = 0; i < size; i++) {
+		putxval(buf[i], 2);
+
+		if ((i & 0xf) == 15) {
+			puts("\n");
+		}
+		else {
+			if ((i & 0xf) == 7) puts(" ");
+			puts(" ");
+		}
+	}
+	puts("\n");
+
+	return 0;
+}
+
+static void wait() {
+	volatile long i;
+	for (i = 0; i < 300000; i++);
+}
+
 int main(void) {
 
+	static char buf[16];
+	static long size = -1;
+	static unsigned char *loadbuf = NULL;
+	extern int buffer_start;		/* リンカスクリプトで定義されたバッファ */
+
 	init();
-	puts("Hello World!\n");
 
-	printval();
-	puts("overwrite variables. \n");
-	global_data = 0x20;
-	global_bss = 0x30;
-	static_data = 0x40;
-	static_bss = 0x50;
-	printval();
+	puts("kzload (kozos boot loader) started.\n");
 
-	while (1);
+	while (1) {
+		puts("kzload> ");	/* プロンプト文字の表示 */
+		gets(buf);			/* シリアルからのコマンドを受信 */
+
+		if (!strcmp(buf, "load")) {
+			loadbuf = (char *)(&buffer_start);
+			size = xmodem_recv(loadbuf);
+			wait();
+			if (size < 0) {
+				puts("\nXMODEM receive error!.\n");
+			}
+			else {
+				puts("\nXMODEM receive succeeded.\n");
+			}
+		}
+		else if (!strcmp(buf, "dump")) {
+			puts("size: ");
+			putxval(size, 0);
+			puts("\n");
+			dump(loadbuf, size);
+		}
+		else if (!strcmp(buf, "run")) {
+			elf_load(loadbuf);
+		}
+		else {
+			puts("unknown.\n");
+		}
+	}
 
 	return 0;
 }
